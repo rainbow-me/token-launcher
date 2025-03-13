c91887eaee7500ad7eb3c59b2b455c27d08afb55
derHowie <derhowiedesigns@gmail.com>
2025-03-13 17:13:35 -0400

    chore: rpc url config cleanup

--- sdk/scripts/start-anvil.sh
+++ sdk/scripts/start-anvil.sh
@@ -3,7 +3,7 @@
 # Source .env but don't fail if variables aren't set
 source .env 2>/dev/null || true
 
-FORK_URL=${TENDERLY_RPC_URL:-"https://virtual.mainnet.rpc.tenderly.co/4a300182-608c-491f-8dd0-3e7fe8d2ec52"}
+FORK_URL=${RPC_URL:-"https://virtual.mainnet.rpc.tenderly.co/4a300182-608c-491f-8dd0-3e7fe8d2ec52"}
 
 echo "Starting anvil with fork-url: ${FORK_URL}"
 anvil --fork-url "$FORK_URL"
