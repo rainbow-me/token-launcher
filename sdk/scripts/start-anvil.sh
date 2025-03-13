21eaef298029038a3139cc783b136e621195e2f9
derHowie <derhowiedesigns@gmail.com>
2025-03-13 16:56:22 -0400

    chore: properly pass in gh secrets

--- sdk/scripts/start-anvil.sh
+++ sdk/scripts/start-anvil.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-source .env
-anvil --fork-url 'https://virtual.mainnet.rpc.tenderly.co/4a300182-608c-491f-8dd0-3e7fe8d2ec52'
+
+# Source .env but don't fail if variables aren't set
+source .env 2>/dev/null || true
+
+# Default RPC URL if not provided in environment
+FORK_URL=${TENDERLY_RPC_URL:-"https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"}
+
+echo "Starting anvil with fork-url: ${FORK_URL}"
+anvil --fork-url "$FORK_URL"
